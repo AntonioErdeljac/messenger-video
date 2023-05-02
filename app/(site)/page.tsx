b3a6d9b7a25f3b73950a44494e1e5ad500a4f11a
Antonio <hedhogs@gmail.com>
2023-05-02 02:56:42 +0200

    03: auth-start

---
+++ app/(site)/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div
+      className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+      "
+    >
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          alt="Logo"
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+        />
+        <h2
+          className="
+            mt-6
+            text-center
+            text-3xl
+            font-bold
+            tracking-tight
+            text-gray-900
+          "
+        >
+          Sign in to your account
+        </h2>
+      </div>
+      {/* AuthForm */}
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
